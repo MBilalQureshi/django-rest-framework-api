6f2eac76faec216e1f92e8ec6d336c24cc50084f
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-21 16:27:52 +0000

    Add Profile modal and register it

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Modal):
+    owner = models.OneToOneField(User, on_delete=modals.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank_true)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_oojj4p'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
