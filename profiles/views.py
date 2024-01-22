6df187020dc84689369fde7ee6fc1c07fdf76f11
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-22 16:41:18 +0000

    Make view to Fetch user based on id and 404 if invalid id

--- profiles/views.py
+++ profiles/views.py
@@ -10,8 +10,37 @@ returning content-negotiated Web API responses  that can be rendered to multiple
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+# now we add our serializer in view to fix 'Object of type Profile is not JSON serializable' bug
+from .serializers import ProfileSerializer
+from django.http import Http404
 
-class ProfileView(APIView):
+class ProfileList(APIView):
+    '''
+    List all profiles
+    No Create View (post method), as profile creation is handled by django "Signals"
+    '''
     def get(self, request):
         profiles = Profile.objects.all()
-        return Response(profiles)
+        '''
+        Before we return a Response, we’ll create  a ProfileSerializer instance.
+        We’ll pass in profiles and many equals True, to specify  we’re serializing multiple Profile instances.
+        Finally, in the Response, we’ll send  data returned from our serializer.
+        '''
+        # many = True means we'll pass multiple profile instances
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    # We need code to handle request for profile id that dosen't exist,(see below)
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        # get single profile based on id so no manyu=true
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
