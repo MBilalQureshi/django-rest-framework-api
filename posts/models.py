cccb5d4ace3ef8f0043169cb952cd9d184a4ed59
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-23 14:12:52 +0000

    Update default cloudinary image and register the Post table

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_oojj4p', blank=True
+        upload_to='images/', default='../default_post_vzxkyq', blank=True
     )
 
     class Meta:
