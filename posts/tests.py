4a9d59803eae355ba29ca4d072c50d419cd57f36
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-23 20:41:42 +0000

    Fail test when logged out user try to create post

--- posts/tests.py
+++ posts/tests.py
@@ -18,8 +18,12 @@ class PostListViewTests(APITestCase):
 
     def test_logged_in_user_can_create_post(self):
         self.client.login(username='adam', password='pass')
+        # create post
         response = self.client.post('/posts/', {'title': 'a title'})
         count = Post.objects.count()
         self.assertEqual(count, 1)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
     
+    def test_user_not_logged_in_cant_create_post(self):
+        response = self.client.post('/post/', {'title': 'a title'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
