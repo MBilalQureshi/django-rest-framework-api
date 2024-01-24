2798230486843047a35994fd248033f913153f70
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-24 22:16:26 +0000

    Refactoor posts and profiles view and keep old files without generics as well

---
+++ posts/viewsWithoutGenerics.py
@@ -0,0 +1,74 @@
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Post
+from .serializers import PostSerializer
+from django.http import Http404
+# Importing the permission we made in drf_api
+from drf_api.permissions import IsOwnerOrReadOnly
+
+class PostList(APIView):
+    # Like we said before below line of code is to beautify the form as per db
+    serializer_class = PostSerializer
+
+    # Adding buit-in permission it's not like ou made permission in drf_api
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly
+    ]
+    def get(self, request):
+        posts = Post.objects.all()
+        serializer = PostSerializer(
+            posts, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(
+            data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                serializer.data, status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+class PostDetail(APIView):
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+    serializer_class = PostSerializer
+
+    def get_object(self, pk):
+        try:
+            post = Post.objects.get(pk=pk)
+            # check if user had permission to add or delete
+            self.check_object_permissions(self.request, post)
+            return post
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request,pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        # SOL is context={'request': request} making request part of context object to access it in serializer.py side
+        serializer = PostSerializer(post, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
---
+++ profiles/viewsWithOutGenerics.py
@@ -0,0 +1,72 @@
+'''
+APIView is very similar to Django’s View  class. It also provides a few bits of extra  
+functionality such as making sure you  receive Request instances in your view,  
+handling parsing errors, and adding  context to Response objects.
+
+Even though we could use Django’s HttpResponse,  
+the Response class is specifically built for the  rest framework, and provides a nicer interface for  
+returning content-negotiated Web API responses  that can be rendered to multiple formats.
+'''
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
+# now we add our serializer in view to fix 'Object of type Profile is not JSON serializable' bug
+from .serializers import ProfileSerializer
+from django.http import Http404
+from rest_framework import status
+# Importing the permission we made in drf_api
+from drf_api.permissions import IsOwnerOrReadOnly
+
+class ProfileList(APIView):
+    '''
+    List all profiles
+    No Create View (post method), as profile creation is handled by django "Signals"
+    '''
+    def get(self, request):
+        profiles = Profile.objects.all()
+        '''
+        Before we return a Response, we’ll create  a ProfileSerializer instance.
+        We’ll pass in profiles and many equals True, to specify  we’re serializing multiple Profile instances.
+        Finally, in the Response, we’ll send  data returned from our serializer.
+        '''
+        # many = True means we'll pass multiple profile instances, SOL is context={'request': request} making request part of context object to access it in serializer.py side
+        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class ProfileDetail(APIView):
+    #    wouldn’t  it be better to have a nice form instead?   
+    # If we explicitly set the serializer_class attribute  on our ProfileDetail view, the rest framework  
+    # will automatically render a form for us, based on  the fields we defined in our ProfileSerializer.
+    # Now, if we save and refresh,  a proper form appears inshort beautify and fix form as per DB.
+    serializer_class = ProfileSerializer
+    
+    #  set the permission_classes  attribute on the ProfileDetail view to an array containing our permission.
+    permission_classes = [IsOwnerOrReadOnly]
+
+    # We need code to handle request for profile id that dosen't exist,(see below)
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            # The last thing we have to do is update the  get_object method by explicitly checking  
+            # object permissions before we return a profile  instance. If the user doesn’t own the profile,  
+            # it will throw the 403 Forbidden  error and not return the instance.
+            self.check_object_permissions(self.request, profile)
+
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        # get single profile based on id so no manyu=true ------ SOL is context={'request': request} making request part of context object to access it in serializer.py side
+        serializer = ProfileSerializer(profile, context={'request': request})
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        profile = self.get_object(pk)
+        # SOL is context={'request': request} making request part of context object to access it in serializer.py side
+        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
