219dc61667e79a7109b5ea545e4252882f15c2b3
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-25 16:52:23 +0000

    Handle if the posts fetched are liked by the current logged in user

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 class PostSerializer(serializers.ModelSerializer):
     # owner: read only field
@@ -10,6 +11,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     # profile_image: a read only field populated with the owner's profile image url.
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    like_id = serializers.SerializerMethodField()
 
     # as the db field that needs to be vlidated is image so method would be validate_image "If we follow this naming convention,  this method will be called automatically  
     # and validate the uploaded image every  time we create or update a post."
@@ -32,10 +35,20 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                # see if currently logged in user is the user who liked the post we are trying to retrieve
+                owner=user, post=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Post
         # In response, inside fields could list them  all in an array or set to fields = '__all__'
         fields = [
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at', 'updated_at', 'title', 'content', 'image',
-            'image_filter'
+            'image_filter', 'like_id'
         ]
