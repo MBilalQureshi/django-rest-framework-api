d88c7954df711fb8d763f26c891442ac748fc6c3
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-25 16:27:36 +0000

    Show in profile list who you are following with following id

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source = 'owner.username')
     # serializerMethodField is read only
     is_owner = serializers.SerializerMethodField()
+
+    # To check who are we following
+    following_id = serializers.SerializerMethodField()
+
     # above is_owner is connected when saying below get_is_owner
     def get_is_owner(self, obj):
         # how to check if rquest.user is same as objects owner, see part of solution in view.py see "SOL" after adding SOL is context={'request': request} we come
@@ -12,11 +17,23 @@ class ProfileSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    # Method to check who we are following
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            # check if user is following any other profiles using filter
+            following = Follower.objects.filter(
+                # if logged in user (owner=user) is following this profile (followed=obj.owner) an instance will be returned else None is returned
+                owner=user, followed=obj.owner
+            ).first()
+            return following.id if following else None
+        # if not authenticated user return None
+        return None
 
     class Meta:
         model = Profile
         # In response, inside fields could list them  all in an array or set to fields = '__all__'
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name', 'content', 'image', 
-            'is_owner'
+            'is_owner', 'following_id'
         ]
